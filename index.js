6c0900b88725a474a669f9076b1d827e1790a8e9
Xianlin <woshihu@hotmail.com>
2019-02-23 16:16:39 -0800

    setup webpack and npm script

---
+++ index.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+
+const firstName = 'Xianlin';
+const lastName = 'Hu'
+console.log(`hello ${firstName} ${lastName}`);
